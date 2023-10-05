25742744dfc312db3a134910f8026977c0c1f08b
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-10-05 16:23:06 +0300

    addNavbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,1 +1,15 @@
 const React = require("react");
+
+function Navbar({user}) {
+  return (
+    <nav>
+    <div className="nav-wrapper">
+      <a href="/regForm" className="brand-logo right">Регистрация</a>
+      <ul id="nav-mobile" className="left hide-on-med-and-down">
+       { user && <li>{`Hello ${user.name}! Ваш результат: ${user.score} `}</li>}
+      </ul>
+    </div>
+  </nav>
+  );
+}
+module.exports = Navbar;

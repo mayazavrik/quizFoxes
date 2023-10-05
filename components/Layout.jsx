9d5e5e1f564f4dbd7eb91eac063e3e94147933c5
mayazavrik <127432774+mayazavrik@users.noreply.github.com>
2023-10-05 16:21:10 +0300

    addLayout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,1 +1,24 @@
 const React = require("react");
+// const Navbar = require("./Navbar");
+
+module.exports = function Layout({ title, children,user }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>{title}</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"
+        />
+
+        {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> */}
+        {/* <link rel="stylesheet" href="/style.css" />
+        <script defer src="/scripts/script.js" /> */}
+      </head>
+      <body>
+        {/* <Navbar user={user}/> */}
+        {children}
+      </body>
+    </html>
+  );
+};
